67004755cd3d5d24cb7bc237f06318988150f381
janarizf <janarizjoyf@gmail.com>
2022-11-24 07:27:00 +0800

    update package

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(9000, () => {
+app.listen(process.env.PORT || 9000, () => {
   console.log('Example app listening on port 9000')
 })
 /* mongoose
