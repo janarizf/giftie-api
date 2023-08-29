a9113ee9cd1f02fe6a5c9e86034cf208959a0d79
janarizf <janarizjoyf@gmail.com>
2023-08-30 03:42:30 +0800

    working lazada and shopee scraper

--- routes/imgscraper copy.js
+++ routes/imgscraper copy.js
@@ -14,7 +14,7 @@ var jsonParser = bodyParser.json();
 
 router.get("/getimg/:url", jsonParser, async function (req, res) {
   const website_url = req.params.url;
-  // const website_url = "https://shopee.ph/Royal-Kludge-RK71-Bluetooth-5.0-USB-C-Dual-Mode-60-71-key-RGB-Backlit-Mechanical-Gaming-Keyboard-Rk71-i.890279864.22902514178";
+  //const website_url = "https://www.lazada.com.ph/products/charge-2-mini-portable-superbass-speaker-splashproof-wireless-bluetooth-speaker-smart-hi-fi-sound-powerful-sound-speaker-i3415034994-s17475253308.html?spm=a2o4l.home.just4u.14.58b7ca18DQgIuX&&scm=1007.17519.162103.0&pvid=8ee7b08d-e378-45ab-9690-d77f4c5a13ec&search=0&clickTrackInfo=pvid%3A8ee7b08d-e378-45ab-9690-d77f4c5a13ec%3Bchannel_id%3A0000%3Bmt%3Ahot%3Bitem_id%3A3415034994%3Bself_ab_id%3A162103%3Bself_app_id%3A7519%3Blayer_buckets%3A5437.25236_955.3631_6059.28889%3Bpos%3A13%3B";
   if (website_url == undefined) {
     res.send("");
   }
@@ -33,18 +33,27 @@ router.get("/getimg/:url", jsonParser, async function (req, res) {
      console.log(imgurl)*/
     // Take screenshot
     try {
-
-
-      var previewImg = await getImg(page, website_url);
-      if (previewImg == null) {
-        const previewData = await linkPreviewGenerator(website_url);
-        previewImg = previewData.img;
+      if (website_url.includes("shopee")) {
+        var shopeeImg = await scrpShopee(page);
+        await browser.close();
+        res.send(shopeeImg);
       }
-
-      await browser.close();
-
-      res.send(previewImg);
-    } catch (error) {
+      else if (website_url.includes("lazada")) {
+        var LazadaImg = await scrpLazada(page);
+        await browser.close();
+        res.send(LazadaImg);
+      }
+      else {
+        const previewData = await linkPreviewGenerator(website_url);
+        var previewImg = previewData.img;
+        if (previewImg == null) {
+            previewImg = await getImg(page, website_url);
+        }
+        await browser.close();
+        res.send(previewImg);
+      }
+    }
+    catch (error) {
       console.log(error)
       res.status(200).send();
     }
@@ -53,52 +62,84 @@ router.get("/getimg/:url", jsonParser, async function (req, res) {
 
 const getImg = async (page, uri) => {
   try {
-    
- 
-  const img = await page.evaluate(async () => {
-    let imgs = Array.from(document.getElementsByTagName("img"));
-    if (imgs.length > 0) {
-      imgs = imgs.filter((img) => {
-        let addImg = true;
-        if (img.naturalWidth > img.naturalHeight) {
-          if (img.naturalWidth / img.naturalHeight > 3) {
-            addImg = false;
-          }
-        } else {
-          if (img.naturalHeight / img.naturalWidth > 3) {
-            addImg = false;
-          }
-        }
-        if (img.naturalHeight <= 200 || img.naturalWidth <= 200) {
-          addImg = false;
-        }
-        console.log("add img");
-        return addImg;
-      });
-      if (imgs.length > 0) {
-        imgs.forEach((img) =>
-          img.src.indexOf("//") === -1
-            ? (img.src = `${new URL(uri).origin}/${img.src}`)
-            : img.src
-        );
-        console.log(imgs);
-        console.log("img length");
-        var num = 0;
-        if (imgs.length > 1)
-          num = 1;
 
-        return imgs[num].src;
+
+    const img = await page.evaluate(async () => {
+      let imgs = Array.from(document.getElementsByTagName("img"));
+      if (imgs.length > 0) {
+        imgs = imgs.filter((img) => {
+          let addImg = true;
+          if (img.naturalWidth > img.naturalHeight) {
+            if (img.naturalWidth / img.naturalHeight > 3) {
+              addImg = false;
+            }
+          } else {
+            if (img.naturalHeight / img.naturalWidth > 3) {
+              addImg = false;
+            }
+          }
+          if (img.naturalHeight <= 200 || img.naturalWidth <= 200) {
+            addImg = false;
+          }
+          console.log("add img");
+          return addImg;
+        });
+        if (imgs.length > 0) {
+          imgs.forEach((img) =>
+            img.src.indexOf("//") === -1
+              ? (img.src = `${new URL(uri).origin}/${img.src}`)
+              : img.src
+          );
+          console.log(imgs);
+          console.log("img length");
+          var num = 0;
+          if (imgs.length > 1)
+            num = 1;
+
+          return imgs[num].src;
+        }
       }
-    }
-    return null;
-  });
-  return img;
-} catch (error) {
+      return null;
+    });
+    return img;
+  } catch (error) {
     console.log(error);
     return null;
-}
+  }
+};
+const scrpShopee = async (page) => {
+  try {
+
+    await page.waitForSelector('div.product-carousel div div.stardust-carousel__item-list-wrapper');
+
+    const textContent = await page.evaluate(() => {
+      return document.querySelector("div.product-carousel div div.stardust-carousel__item-list-wrapper ul li:nth-child(1) div div img").src;
+    });
+
+    return textContent;
+  }
+  catch (error) {
+    console.log(error);
+    return null;
+
+  }
 };
 
+const scrpLazada = async (page) => {
+  try {
+    await page.waitForSelector('#module_item_gallery_1 div div.gallery-preview-panel');
+    const textContent = await page.evaluate(() => {
+      return document.querySelector('div.gallery-preview-panel div img.pdp-mod-common-image.gallery-preview-panel__image').src;
+    });
+
+    return textContent;
+  }
+  catch (error) {
+    console.log(error);
+    return null;
+
+  }
+};
 /*(async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
