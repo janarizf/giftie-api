6880f9b65173eebd7417c9eafba27f5c2f98a1c0
janarizf <janarizjoyf@gmail.com>
2023-08-30 18:00:06 +0800

    puppeteer and cors versions

--- app.js
+++ app.js
@@ -35,17 +35,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-//app.use(cors());
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested, Content-Type, Accept Authorization"
+//   )
+//   res.header('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 app.use(bodyParser.urlencoded({limit: '25mb', extended: true }));
 app.use(express.json({limit: '25mb'}));
 app.use(cookieParser());
