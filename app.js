c89e6eeb8a8909b66d3113d737edecad00f571b9
janarizf <janarizjoyf@gmail.com>
2023-08-30 08:10:52 +0800

    added cors settings

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(bodyParser.urlencoded({limit: '25mb', extended: true }));
 app.use(express.json({limit: '25mb'}));
 app.use(cookieParser());
