6072977b810f992411fcee9b9d08aeb0cd266bee
janarizf <janarizjoyf@gmail.com>
2023-08-30 17:09:01 +0800

    cors

--- app.js
+++ app.js
@@ -37,10 +37,13 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 //app.use(cors());
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 app.use(bodyParser.urlencoded({limit: '25mb', extended: true }));
